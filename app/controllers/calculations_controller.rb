3e07ff7bdb70b5f8867d0a4d70306cc3a1743e6b
Collin  Jones <collinpjones@users.noreply.github.com>
2016-11-07 17:27:16 -0600

    Random Complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -12,4 +12,10 @@ class CalculationsController < ApplicationController
     @number = params[:number].to_f
     @square = (@number ** 2).round(2)
   end
+
+  def random
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @random = rand(@min..@max)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   #Square Root
   get("/square_root/:number", { :controller => "calculations", :action => "square_root"})
 
+  #Square
   get("/square/:number", { :controller => "calculations", :action => "square"})
+
+  #Random
+  get("/random/:min/:max", { :controller => "calculations", :action => "random"})
+
 end
